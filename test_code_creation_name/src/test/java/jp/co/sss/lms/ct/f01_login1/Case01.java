1b9482c896eaf337cd58dcc8e2954c030afc30fb
s-watanabe <s-watanabe@ISI-TA25L08>
2026-09-18 17:33:40 +0900

    add:Case01.ログイン画面への遷移テストコード追加 WebElementを利用してタイトルの値とログインボタンの値を取得し、それぞれ期待値通りかどうか確認するテストを作成いた。 期待値は以下の通り タイトル：ログイン ログインボタン：ログイン

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,7 +36,16 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//URLにアクセスする
+		goTo("http://localhost:8080/lms/");
+
+		//画面遷移が成功したかを確かめるため、タイトルタグを取得・検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンスを取得（テストNo.1）
+		getEvidence(new Object() {
+		});
 	}
 
 }
