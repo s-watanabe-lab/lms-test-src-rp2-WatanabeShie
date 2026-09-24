402b9e7587f68e5ab4d6b67cd10a0ecee88f1c65
s-watanabe <s-watanabe@ISI-TA25L08>
2026-09-24 14:33:09 +0900

    fix: テスト仕様書およびCase02テストコードの指摘事項修正
    ・テスト仕様書の表紙の更新履歴内容を具体的に修正
    ・ケース02のテスト項目をケース一覧の概要を満たす構成（画面表示・DB未登録ログイン）に修正
    ・仕様書修正に伴い、Case02.javaの不要コード（未入力チェック）の削除および修正
    ・不要になったエビデンス画像の整理

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -3,8 +3,6 @@ package jp.co.sss.lms.ct.f01_login1;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -38,112 +36,23 @@ public class Case02 {
 
 	@Test
 	@Order(1)
-	@DisplayName("テスト01 ログインID未入力")
+	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-
-		// ブラウザをリセット
-		webDriver.manage().deleteAllCookies();
 
 		//URLにアクセスする
 		goTo("http://localhost:8080/lms/");
 
-		// ログインID欄はクリアにする
-		webDriver.findElement(By.id("loginId")).clear();
-
-		//入力するパスワード
-		webDriver.findElement(By.id("password")).clear();
-		webDriver.findElement(By.id("password")).sendKeys("StudentAA001");
-
-		//ログインボタンをクリック
-		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
-
-		//検証：エラーメッセージが表示されているか
-		WebElement errorMsg = webDriver.findElement(By.className("error"));
-
-		// メッセージが画面に表示されているか（CSS等で隠れていないか）
-		assertTrue(errorMsg.isDisplayed());
-
-		// メッセージの内容が正しいか
-		assertTrue(errorMsg.getText().contains("ログインIDは必須です。"));
-
-		//エビデンスを取得（テスト01）
+		//画面遷移が成功したかを確かめるため、タイトルタグを取得・検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//エビデンスを取得（テストNo.1）
 		getEvidence(new Object() {
 		});
 	}
 
 	@Test
 	@Order(2)
-	@DisplayName("テスト02 パスワード未入力")
+	@DisplayName("テスト04 DBに存在しないログインID・パスワードを入力")
 	void test02() {
-
-		// ブラウザをリセット
-		webDriver.manage().deleteAllCookies();
-
-		//URLにアクセスする
-		goTo("http://localhost:8080/lms/");
-
-		//入力するログインID
-		webDriver.findElement(By.id("loginId")).clear();
-		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA001");
-
-		// パスワード欄はクリアにする
-		webDriver.findElement(By.id("password")).clear();
-
-		//ログインボタンをクリック
-		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
-
-		//検証：エラーメッセージが表示されているか
-		WebElement errorMsg = webDriver.findElement(By.className("error"));
-
-		// メッセージが画面に表示されているか（CSS等で隠れていないか）
-		assertTrue(errorMsg.isDisplayed());
-
-		// メッセージの内容が正しいか
-		assertEquals("パスワードは必須です。", errorMsg.getText());
-
-		//エビデンスを取得（テスト01）
-		getEvidence(new Object() {
-		});
-	}
-
-	@Test
-	@Order(3)
-	@DisplayName("テスト03 ログインID・パスワード未入力")
-	void test03() {
-
-		// ブラウザをリセット
-		webDriver.manage().deleteAllCookies();
-
-		//URLにアクセスする
-		goTo("http://localhost:8080/lms/");
-
-		//未入力の欄はクリアにする
-		webDriver.findElement(By.id("loginId")).clear();
-		webDriver.findElement(By.id("password")).clear();
-
-		//未入力の状態でログインボタンをクリック
-		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
-
-		//1つ目のエラーが出るまで画面更新を待つ
-		webDriver.findElement(By.className("error"));
-
-		//検証：画面更新後に全てのエラーメッセージが表示されているか
-		List<WebElement> errorMsg = webDriver.findElements(By.className("error"));
-
-		// メッセージが画面に表示されているか（CSS等で隠れていないか）
-		assertTrue(errorMsg.get(0).getText().contains("ログインIDは必須です。"));
-		assertTrue(errorMsg.get(1).getText().contains("パスワードは必須です。"));
-
-		//エビデンスを取得（テスト01）
-		getEvidence(new Object() {
-		});
-
-	}
-
-	@Test
-	@Order(4)
-	@DisplayName("テスト04 誤ったログインID・パスワードを入力")
-	void test04() {
 
 		// ブラウザをリセット
 		webDriver.manage().deleteAllCookies();
@@ -168,75 +77,6 @@ public class Case02 {
 		// メッセージの内容が正しいか
 		assertTrue(errorMsg.isDisplayed());
 		assertTrue(errorMsg.getText().contains("ログインに失敗しました。"));
-
-		//エビデンスを取得（テスト01）
-		getEvidence(new Object() {
-		});
-	}
-
-	@Test
-	@Order(5)
-	@DisplayName("テスト05 境界値：ロック直前（2回連続失敗）")
-	void test05() {
-
-		//test04までの失敗カウントをリセット
-		webDriver.manage().deleteAllCookies();
-
-		//URLにアクセスする
-		goTo("http://localhost:8080/lms/");
-
-		//繰り返し処理
-		for (int i = 0; i < 2; i++) {
-
-			//①ID欄に入力
-			webDriver.findElement(By.id("loginId")).clear();
-			webDriver.findElement(By.id("loginId")).sendKeys("StudentA300");
-
-			//②PW欄に入力
-			webDriver.findElement(By.id("password")).clear();
-			webDriver.findElement(By.id("password")).sendKeys("StudentA300");
-
-			//ボタンを押す
-			webDriver.findElement(By.cssSelector("input[type='submit']")).click();
-		}
-
-		//検証：エラーメッセージが表示されているか
-		WebElement errorMsg = webDriver.findElement(By.className("error"));
-
-		// メッセージの内容が正しいか
-		assertTrue(errorMsg.getText().contains("ログインに失敗しました。"));
-
-		//エビデンスを取得（テスト01）
-		getEvidence(new Object() {
-		});
-	}
-
-	@Test
-	@Order(6)
-	@DisplayName("テスト06 境界値：ロック発生（3回連続失敗）")
-	void test06() {
-		//URLにアクセスする
-		goTo("http://localhost:8080/lms/");
-
-		//繰り返し処理
-		for (int i = 0; i < 3; i++) {
-			//①ID欄に入力
-			webDriver.findElement(By.id("loginId")).clear();
-			webDriver.findElement(By.id("loginId")).sendKeys("StudentA300");
-
-			//②PW欄に入力
-			webDriver.findElement(By.id("password")).clear();
-			webDriver.findElement(By.id("password")).sendKeys("StudentA300");
-
-			//ボタンを押す
-			webDriver.findElement(By.cssSelector("input[type='submit']")).click();
-		}
-
-		//検証：エラーメッセージが表示されているか
-		WebElement errorMsg = webDriver.findElement(By.className("error"));
-
-		// メッセージの内容が正しいか
-		assertTrue(errorMsg.getText().contains("規定の回数を超えたため、アカウントにロックがかかりました。"));
 
 		//エビデンスを取得（テスト01）
 		getEvidence(new Object() {
