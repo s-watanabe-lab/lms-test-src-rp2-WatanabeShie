b6368c271e6408b535000d273ee9a4daec31175d
s-watanabe <s-watanabe@ISI-TA25L08>
2026-09-24 13:48:53 +0900

    add:Case03.java 受講生 ログイン 正常系のテストコード作成
    ログイン画面において、初回ログイン受講生ユーザーおよび初回ログイン済み受講生ユーザーのログイン処理と画面遷移テストを作成。
    WebDriverUtils.visibilityTimeoutを利用して画面読み込み完了を待機し、タイトルおよび遷移先の検証を実施。
    
    期待値は以下の通り：
    ・テスト01（初回ログイン）：ログイン成功後、利用規約同意画面へ遷移すること
    ・テスト02（初回ログイン済みログイン）：ログイン成功後、コース詳細画面へ遷移すること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,67 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// ブラウザをリセット
+		webDriver.manage().deleteAllCookies();
+
+		//URLにアクセスする
+		goTo("http://localhost:8080/lms/");
+
+		//入力するログインID
+		webDriver.findElement(By.id("loginId")).clear();
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+
+		//入力するパスワード
+		webDriver.findElement(By.id("password")).clear();
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		//Utilの機能より、規約画面の「securityFlg」が表示されるまで最大5秒待機する
+		visibilityTimeout(By.name("securityFlg"), 5);
+
+		//画面遷移が成功したかを確かめるため、タイトルタグを取得・検証
+		assertTrue(webDriver.getTitle().contains("セキュリティ規約"));
+
+		//エビデンスを取得（テストNo.1）
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ブラウザをリセット
+		webDriver.manage().deleteAllCookies();
+
+		//URLにアクセスする
+		goTo("http://localhost:8080/lms/");
+
+		//入力するログインID
+		webDriver.findElement(By.id("loginId")).clear();
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		//入力するパスワード
+		webDriver.findElement(By.id("password")).clear();
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA011");
+
+		//ログインボタンをクリック
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		//Utilの機能より、コース詳細画面の「h2」が表示されるまで最大5秒待機する
+		visibilityTimeout(By.tagName("h2"), 5);
+
+		//画面遷移が成功したかを確かめるため、タイトルタグを取得・検証
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンスを取得（テストNo.1）
+		getEvidence(new Object() {
+		});
 	}
 
 }
